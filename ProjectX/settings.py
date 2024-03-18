2170d138f8cc0dbeb1e9d8e4a0fcea77ce2d3d14
abhishekdubey1412 <abhishekdubey1412@gmail.com>
2024-03-18 16:32:14 +0530

    modify

--- ProjectX/settings.py
+++ ProjectX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ee(cxe6g2xmw162-c0*3l-%ep0=z++n62-#vw@p%shp9376mk2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
